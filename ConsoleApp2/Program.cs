2d4f9804b3466f7fc0f8f36dd06cc555cfbaa01d
YulianaPetriuk <159484811+YulianaPetriuk@users.noreply.github.com>
2024-10-08 22:47:02 +0300

    commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        // Запитуємо користувача ввести рік
+        Console.WriteLine("Введіть рік:");
+        int year = int.Parse(Console.ReadLine());
+
+        // Перевіряємо, чи є рік високосним
+        if (IsLeapYear(year))
+        {
+            Console.WriteLine($"{year} є високосним роком.");
+        }
+        else
+        {
+            Console.WriteLine($"{year} не є високосним роком.");
+        }
+    }
+
+    // Метод для перевірки, чи є рік високосним
+    static bool IsLeapYear(int year)
+    {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
