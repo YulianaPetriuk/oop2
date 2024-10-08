542ab4d731bb9b4fdba479396aecef28fa4ffd7a
YulianaPetriuk <159484811+YulianaPetriuk@users.noreply.github.com>
2024-10-08 22:58:26 +0300

    commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запитуємо у користувача вік
+        Console.WriteLine("Введіть ваш вік:");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        // Перевіряємо, чи є вік підлітковим (від 12 до 18 років)
+        if (age >= 12 && age <= 18)
+        {
+            Console.WriteLine("Ваш вік підлітковий.");
+        }
+        else
+        {
+            Console.WriteLine("Ваш вік не підлітковий.");
+        }
+    }
+}
