708c60d7d4e33318ce54489dd7c5efdc8fc33f45
YulianaPetriuk <159484811+YulianaPetriuk@users.noreply.github.com>
2024-10-08 23:03:38 +0300

    commit

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,35 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        // Запитуємо у користувача довжини трьох сторін трикутника
+        Console.WriteLine("Введіть довжину першої сторони (a):");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть довжину другої сторони (b):");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть довжину третьої сторони (c):");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        // Визначаємо найдовшу сторону
+        double longest = a;
+        string longestSide = "a";
+
+        if (b > longest)
+        {
+            longest = b;
+            longestSide = "b";
+        }
+
+        if (c > longest)
+        {
+            longest = c;
+            longestSide = "c";
+        }
+
+        // Виводимо результат
+        Console.WriteLine($"Найдовша сторона: {longestSide}, її довжина: {longest}");
+    }
+}
