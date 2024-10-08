fcbc1779d1c8090b6e29bed2cc6def03a6ab86cc
YulianaPetriuk <159484811+YulianaPetriuk@users.noreply.github.com>
2024-10-08 23:00:35 +0300

    commit

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запитуємо у користувача число
+        Console.WriteLine("Введіть число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Перевіряємо, чи є число парним або непарним
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Число парне.");
+        }
+        else
+        {
+            Console.WriteLine("Число непарне.");
+        }
+
+        // Перевіряємо, чи ділиться число на 3
+        if (number % 3 == 0)
+        {
+            Console.WriteLine("Число ділиться на 3.");
+        }
+        else
+        {
+            Console.WriteLine("Число не ділиться на 3.");
+        }
+    }
+}
